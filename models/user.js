dbe8991590d1f83dea035c1771f2f2985289ab09
Matthew Campbell <m.campbell92@gmail.com>
2025-02-26 14:44:19 +0100

    change user const to lower case

--- models/user.js
+++ models/user.js
@@ -20,5 +20,5 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
-export default User;
+const user = mongoose.model("User", userSchema);
+export default user;
