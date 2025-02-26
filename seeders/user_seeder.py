f66946a9149543800d011fd71fddf7777bb02632
Matthew Campbell <m.campbell92@gmail.com>
2025-02-26 18:51:22 +0100

    move try statement up to encapsulate all function logic:

--- seeders/user_seeder.py
+++ seeders/user_seeder.py
@@ -1,8 +1,8 @@
 import os
-import pymongo
 from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
 from faker import Faker
+from datetime import datetime
 
 load_dotenv()
 fake = Faker()
@@ -11,26 +11,29 @@ uri = os.getenv("MONGO_URI")
 
 def main():
     
-    client = pymongo.MongoClient(uri)
-    db = client['test']
-    users = db['users']
+    try:
+        client = MongoClient(uri)
+        db = client['test']
+        users = db['users']
 
-SEED_DATA = []
-for _ in range(100):
-    SEED_DATA.append({
-    'name': fake.name(),
-    'email': fake.email(),
-    'password': fake.password(),
-})
+        SEED_DATA = []
+        for _ in range(10):
+            SEED_DATA.append({
+            'name': fake.name(),
+            'email': fake.email(),
+            'password': fake.password(),
+            'createdAt': datetime.now()
+        })
+            
+        seeded = users.insert_many(SEED_DATA)
+        if (seeded):
+            print("Data successfully seeded")
 
-seeded = users.insert_many(SEED_DATA)
-
-
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+            client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+        
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
